11c36b2b3ce283ef014c2d4d7e873b81f2acc50a
Eric Murphy <ericnmurphy@gmail.com>
2018-09-24 22:33:43 +0700

    Added json as database

--- app.js
+++ app.js
@@ -1,69 +1,36 @@
 const Jimp = require('jimp')
 const Unsplash = require('unsplash-js').default
-const Storage = require('@google-cloud/storage')
-const firebase = require('firebase')
-const admin = require('firebase-admin')
-require('firebase/firestore')
 //node fetch polyfill
 require('es6-promise').polyfill()
 require('isomorphic-fetch')
-
-const serviceAccount = require('./sdkKey.json')
+var fs = require('fs')
 
 //api keys
 const keys = require('./keys')
 
+const saveMetadata = (author, attrUrl, slug) => {
+  const database = 'client/metadata.json'
+  const metadata = [{ author, attrUrl, url: `./images/${slug}.jpg` }]
+  const json = JSON.parse(fs.readFileSync(database))
+  json.push(...metadata)
+
+  fs.writeFile(database, JSON.stringify(json))
+}
+
 //bokeh-fy images
 const blurImage = (url, slug) => {
   return Jimp.read(url)
-    .then(function(image) {
+    .then(image => {
       return image
         .blur(150)
         .quality(80)
         .scaleToFit(2560, 2560)
-        .write(`images/${slug}.jpg`)
+        .write(`client/images/${slug}.jpg`)
     })
-    .catch(function(err) {
+    .catch(err => {
       console.log(err)
     })
 }
-
-//upload to firebase
-const upload = slug => {
-  const filename = `images/${slug}.jpg`
-
-  storage
-    .bucket(bucketName)
-    .upload(filename)
-    .then(filename => {
-      console.log(`${filename} uploaded to ${bucketName}.`)
-      // getPublicUrl(filename);
-    })
-    .catch(err => {
-      console.error('ERROR:', err)
-    })
-}
-
-// const getPublicUrl = filename => {
-//   storage
-//     .bucket(bucketName)
-//     .file(filename)
-//     .getSignedUrl({
-//       action: "read",
-//       expires: "03-09-2491"
-//     })
-//     .then(signedUrls => {
-//       console.log(signedUrls[0]);
-//     });
-// };
-
-// const uploadMeta = slug => {
-//   const docRef = db.collection("images").doc(slug);
-//   const setImage = docRef.set({
-//     author: author,
-//     attrUrl: attrUrl
-//   });
-// };
 
 //unsplash setup
 const unsplash = new Unsplash({
@@ -77,19 +44,14 @@ unsplash.photos
   .getRandomPhoto({ collections: ['1065396', '1065412'] })
   .then(res => res.json())
   .then(json => {
-    console.log(json)
     const url = json.urls.full
     const author = json.user.name
-    const attrUrl = `${
-      json.links.html
-    }?utm_source=your_app_name&utm_medium=referral`
+    const attrUrl = `${json.links.html}?utm_source=unbokeh&utm_medium=referral`
     const slug = `${json.user.username}-${json.id}-unbokeh`.toLowerCase()
 
-    async function processData() {
-      await blurImage(url, slug)
-      await upload(slug)
-      // uploadMeta();
-    }
+    // add metadata to database
+    saveMetadata(author, attrUrl, slug)
 
-    processData()
+    // blur the damn image!
+    blurImage(url, slug)
   })
